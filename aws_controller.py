43233e8e42ce3766d823f20d1d044af2b73c921d
root <root@LAPTOP-TUFOK6QS.localdomain>
2020-04-18 18:22:16 +0100

    removed sub dir

---
+++ aws_controller.py
@@ -0,0 +1,20 @@
+import boto3
+
+dynamo_client = boto3.client('dynamodb', region_name='eu-west-1')
+
+
+def get_items():
+    return dynamo_client.scan(
+        TableName='MessageBoard'
+    )
+
+
+def put_items(name,message):
+    dynamo_client.put_item(
+        TableName='MessageBoard',
+        Item={
+            'User':{'S': name},
+            'Comments':{'S':message}
+            }
+    )
+
