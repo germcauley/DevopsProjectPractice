3c0b974f8af34d9e2619e142d3ba505378745176
Gerald McAuley <geraldmcauley@gmail.com>
2020-04-20 23:45:30 +0100

    Update selenium_tests.py

--- selenium_tests.py
+++ selenium_tests.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 
-driver = webdriver.Chrome("chromedriver/")
+driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
 
 # Visit my app hosted on aws
 driver.get("http://devopsassignment-env.eba-mx9qvtf8.eu-west-1.elasticbeanstalk.com/")
