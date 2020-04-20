b75ce35f8ab821efd1cc7874850485f974add7cd
root <root@LAPTOP-TUFOK6QS.localdomain>
2020-04-20 23:01:14 +0100

    add automation, update workflow

---
+++ selenium_tests.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+driver = webdriver.Chrome("chromedriver/chromedriver.exe")
+
+# Visit my app hosted on aws
+driver.get("http://devopsassignment-env.eba-mx9qvtf8.eu-west-1.elasticbeanstalk.com/")
+
+title = driver.find_element_by_tag_name('h1')
+messagelink = driver.find_element_by_tag_name('h1')
+time.sleep(5)
+
+assert title.text =="Welcome to Message app"
+
+# assert messagelink.text == "Hello"
+driver.close()
