dd11f331327d2d07b43ef0b6ec0f56d0a0b7ca2c
root <root@LAPTOP-TUFOK6QS.localdomain>
2020-04-20 20:00:26 +0100

    add unittests

--- test_app.py
+++ test_app.py
@@ -5,34 +5,29 @@ import aws_controller
 
 class TestStringMethods(unittest.TestCase):
 
-<<<<<<< HEAD
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOxO')
-=======
-    assert response.status_code == 200
-    
->>>>>>> 80f2c21dca0b5ff46fb73b3bd82a614cacc291d5
 
+    # Some basic unittests
 
-    # def test_apphomepage(self):
-    #     response = app.test_client().get('/')
+    def test_apphomepage(self):
+        response = app.test_client().get('/')
 
-    #     assert response.status_code == 404
+        self.assertEqual(response.status_code,200)
         
 
-    # def test_appcommentpage1():
-    #     response = app.test_client().get('/post')
+    def test_appcommentpage1(self):
+        response = app.test_client().get('/post')
 
-    #     assert response.status_code == 200
+        self.assertEqual(response.status_code,200)
 
-    # def test_appcommentpage2():
+    # def test_appcommentpage2(self):
     #     response = app.test_client().post('/post')
 
-    #     assert response.status_code == 500
+    #     self.assertEqual(response.status_code,500)
 
-    # def test_appsuccesspage1():
-    #     response = app.test_client().get('/success')
+    def test_appsuccesspage1(self):
+        response = app.test_client().get('/success')
 
-    #     assert response.status_code == 200
+        self.assertEqual(response.status_code,200)
+
 if __name__ == '__main__':
     unittest.main()
