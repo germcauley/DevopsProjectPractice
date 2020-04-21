cb8e2c0fc98bc8059072e663936402ff30efe2f2
Gerald McAuley <geraldmcauley@gmail.com>
2020-04-21 01:15:18 +0100

    Update selenium_tests.py

--- selenium_tests.py
+++ selenium_tests.py
@@ -4,7 +4,6 @@ import time
 
 
 options = webdriver.ChromeOptions()
-options.binary_location = '/opt/google/chrome/google-chrome'
 options.add_argument('--headless')
 # service_log_path = "{}/chromedriver.log".format(outputdir)
 # service_args = ['--verbose']
